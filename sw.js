124de5ef1a4285d80d40abef4fe65c07f65dfb5e
vohco-cmd <vohco1570@gmail.com>
2026-09-24 11:17:02 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='color-accent-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate' || new URL(e.request.url).pathname.endsWith('/index.html')) {
+    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return r}).catch(()=>caches.match('./index.html')));
+  } else {
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
+});
